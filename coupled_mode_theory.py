15b1055347387f6c503a9d3bcf3693c479414061
ompaiano <182190352+ompaiano@users.noreply.github.com>
2025-05-22 22:42:38 -0300

    basic functions for coupled mode theory.

---
+++ coupled_mode_theory.py
@@ -0,0 +1,196 @@
+"""See Okamoto pag. 179.
+
+"""
+
+import numpy as np
+from numpy import pi
+from scipy import optimize
+import matplotlib.pyplot as plt
+
+
+def kx_system(ks, p, a, n0, n1, k):
+    eq1 = (p-1)*pi/2.0 - ks[0]*a + np.atan( (n1**2 *ks[1])/(n0**2 *ks[0]) )
+    eq2 = k**2  * (n1**2 - n0**2) - ks[0]**2 - ks[1]**2
+    return [eq1, eq2] 
+
+def ky_system(ks, p, a, n0, n1, k):
+    eq1 = (p-1)*pi/2.0 - ks[0]*a + np.atan(ks[1]/ks[0])
+    eq2 = k**2  * (n1**2 - n0**2) - ks[0]**2 - ks[1]**2
+    return [eq1, eq2] 
+
+def calc_kappa(k, kx, gx, a, n0, n1):
+    """Calculate kappa (codirectional mode coupling in coupled mode theory) 
+    for the particular case of eq. 4.91 of Okamoto's 'Fundamentals of Optical 
+    Waveguides'.
+
+    This method is approximate and applies for waveguides and mode satisfying 
+    the following assumptions: 
+        1. $E^x_{11}$ mode.
+        2. rectangular waveguide.
+
+    Parameters
+    ----------
+    k: float
+        Wavenumber (in vacuum).
+    kx: float
+        Componente of the effective wave-vector in the x direction.
+    gx: float
+        Exponential decay wavenumber (check the actual name for this parameter).
+        for the x direction.
+    a: float
+        Half of the waveguide width in the x direction.
+    n0: float
+        Cladding refractive index.
+    n1: float
+        Core refractive index.
+    gap: float
+        Gap between the two waveguides.
+
+    Returns
+    -------
+    float
+    
+    """
+    delta = 0.5 * (n1**2 - n0**2) / n1**2 
+    normalized_frequency = k * n1 * a * np.sqrt(2*delta)
+    kappa = np.sqrt(2*delta) * a * (a* kx * gx)**2 * np.exp(-gx*gap)
+    kappa /= (1 + gx*a) * normalized_frequency**3
+    return kappa, delta, normalized_frequency
+
+def calc_coupling_length(kappa, delta):
+    """Calculate the length for maximum power transfer bewteen waveguides.
+
+    """
+    Lc = 0.5*pi / np.sqrt(kappa**2 + delta**2)
+    return Lc
+
+def sanity_check_compare_against_okamotos_results():
+
+    pass
+
+def calc_coupling_length_vs_gap(
+    n0, n1, width, height, gap, lambida, p, q):
+    """
+
+    """
+    k = 2*pi / lambida # wavenumber (for vacuum).
+    n_avg = np.mean((n0, n1))
+    ksx0 = (n_avg*k, k)
+    ksy0 = (n_avg*k, k)
+
+    solx = optimize.root(kx_system, ksx0, args=(p, width/2, n0, n1, k))
+    soly = optimize.root(ky_system, ksy0, args=(q, height/2, n0, n1, k))
+    kx, gamma_x = solx.x
+    ky, gamma_y = soly.x
+
+    print("Status da solução na direção x: ", solx.success, "\n\tMessage: ", solx.message)
+    print("Status da solução na direção y: ", soly.success, "\n\tMessage: ", soly.message)
+
+    # Print the solution
+    print("kx/k, gamma_x/k: ", kx/k, gamma_x/k)
+    print("ky/k, gamma_y/k: ", ky/k, gamma_y/k)
+
+    print(kx*width/2, gamma_x*width/2)
+
+    beta = np.sqrt( (k*n1)**2 - kx**2 - ky**2 )
+    print("effective index (beta/k): ", beta/k)
+
+    kappa, deltao, norm_freq = calc_kappa(k, kx, gamma_x, width/2, n0, n1)
+    print("kappa, deltao, norm_freq: ", kappa, deltao, norm_freq)
+
+    coupling_length = calc_coupling_length(kappa, delta=0)
+    print("Coupling length [micrometers]: ", coupling_length*1e6)
+
+    return coupling_length
+
+def calc_max_power_coupling_efficiency(kappa, delta):
+    """Calculate the maximum power coupling efficiency.
+
+    """
+    F = 1 / (1 + (delta/kappa)**2)
+    return F
+
+def calc_optical_power_evolution(z, kappa, delta):
+    F = calc_max_power_coupling_efficiency(kappa, delta)
+    q = np.sqrt( kappa**2 + delta**2 )
+    Pa = 1 - F * np.sin(q*z)**2
+    Pb = F * np.sin(q*z)**2
+    return Pa, Pb
+
+# Waveguide parameters:
+n0 = 1.45 # silica refractive index.
+n1 = 3.48 # silicon refractive index.
+width = 500e-9  # meters.
+height = 220e-9 # meters.
+gap = 75e-9
+
+# Optical parameters:
+lambida = 1560e-9  # meters.
+k = 2*pi / lambida # wavenumber (for vacuum).
+p = 1
+q = 1
+
+# Okamoto parameters (sanity check, it works!):
+# width = 8e-6
+# height = width
+# n0 = 1.45
+# n1 = 1.4544
+# D = 3*width/2
+# gap = D - width
+# lambida = 1550e-9  # meters.
+# k = 2*pi / lambida # wavenumber (for vacuum).
+# p = 1
+# q = 1
+
+a_avg = np.mean((width, height)) / 2
+n_avg = np.mean((n0, n1))
+ksx0 = (n_avg*k, 1/a_avg)
+ksy0 = (n_avg*k, 1/a_avg)
+
+ksx0 = (n_avg*k, k)
+ksy0 = (n_avg*k, k)
+
+solx = optimize.root(kx_system, ksx0, args=(p, width/2, n0, n1, k))
+soly = optimize.root(ky_system, ksy0, args=(q, height/2, n0, n1, k))
+kx, gamma_x = solx.x
+ky, gamma_y = soly.x
+print("Status da solução na direção x: ", solx.success, "\n\tMessage: ", solx.message)
+print("Status da solução na direção y: ", soly.success, "\n\tMessage: ", soly.message)
+
+# Print the solution
+print("kx/k, gamma_x/k: ", kx/k, gamma_x/k)
+print("ky/k, gamma_y/k: ", ky/k, gamma_y/k)
+print(kx*width/2, gamma_x*width/2)
+
+beta = np.sqrt( (k*n1)**2 - kx**2 - ky**2 )
+print("effective index (beta/k): ", beta/k)
+
+
+# Coupled mode theory stuff
+kappa, deltao, norm_freq = calc_kappa(k, kx, gamma_x, width/2, n0, n1)
+print("kappa, deltao, norm_freq: ", kappa, deltao, norm_freq)
+
+coupling_length = calc_coupling_length(kappa, delta=0)
+print("Coupling length [micrometers]: ", coupling_length*1e6)
+
+## assumir um erro de fabricação na diferenca de largura dos guias e ver como isso muda o max power couplinf efficiency.
+
+
+gaps = np.linspace(20e-9, 400e-9, 50)
+Lc = []
+for gap in gaps:
+    coupling_length = calc_coupling_length_vs_gap(n0, n1, width, height, gap, lambida, p, q)
+    Lc.append(coupling_length)
+Lc = np.array(Lc)
+
+plt.plot(gaps*1e9, Lc*1e6)
+plt.xlabel("gap [nm]")
+plt.ylabel("$L_c$ [$\\mu$m]")
+plt.grid(which="both")
+plt.grid(which="minor", alpha=.25, color="k", linestyle="--", linewidth=.5)
+plt.minorticks_on()
+plt.savefig("Lc_vs_gap.pdf")
+plt.show()
+
+# z = np.linspace(0.0, 10e-6, 1000)
+# Pa, Pb = calc_optical_power_evolution(z, kappa, delta)
